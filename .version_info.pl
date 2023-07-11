a8548f635d2d658aac7310421b7d262c2d1e27e1
Alex Baranowski <aleksander.baranowski@yahoo.pl>
2023-07-11 18:18:48 +0200

    Frozen autocommit 2023-07-11 18:18:48.948930

---
+++ .version_info.pl
@@ -0,0 +1,9 @@
+# .version_info.pl for File-Path
+%trans = (
+    version => { old => '2.18_001', new => '2.18' },
+    date    => { old => 'November 4 2020', new => 'November 4 2020' },
+    copyright => { old => '2020', new => '2020' },
+);
+
+1;
+
